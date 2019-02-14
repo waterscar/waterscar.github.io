b6a760ecc765874800fd1237b8d2d6605fc9a1de
Edward Xie <edward.xie@leandev.se>
2019-02-14 18:21:32 +0800

    append look and feel from eAccounting VFAB-32 related

--- resources/js/login.js
+++ resources/js/login.js
@@ -5,3 +5,21 @@ function login() {
     }
     return false;
 }
+
+$(function(){
+    var input = $('#password');
+
+// Execute a function when the user releases a key on the keyboard
+    input.keypress(function (event) {
+        // Cancel the default action, if needed
+        var keycode = (event.keyCode ? event.keyCode : event.which);
+        // Number 13 is the "Enter" key on the keyboard
+        if (keycode === 13) {
+            // Trigger the button element with a click
+            $("#loginBtn").click();
+        }
+    });
+
+
+});
+
